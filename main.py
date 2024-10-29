8d677a73441eeacf786542fb5392de25da5b5b88
hruntov <hruntov@outlook.com>
2024-10-29 16:30:33 +0200

    Add alowwed users to the config files with using ids or usernames

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ config.read('config.ini')
 TELEGRAM_BOT_TOKEN = config['telegram']['bot_token']
 TELEGRAM_TIMEOUT = int(config['telegram']['timeout'])
 DOWNLOADS_FOLDER = config['paths']['downloads_folder']
+ALLOWED_USERS_IDS = [
+    int(user_id.strip()) for user_id in config['telegram']['allowed_users_ids'].split(',')]
+ALLOWED_USERS_USERNAMES = [
+    username.strip() for username in config['telegram']['allowed_users_usernames'].split(',')]
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN, timeout=TELEGRAM_TIMEOUT)
 dp = Dispatcher()
@@ -26,18 +30,48 @@ async def send_welcome(message: types.Message):
 
 
 @router.message(lambda message: message.content_type == ContentType.DOCUMENT)
-async def handle_document(message: types.Message):
-    file_info = await bot.get_file(message.document.file_id)
-    file_path = file_info.file_path
-    file_name = message.document.file_name
+async def handle_document(message: types.Message) -> None:
+    """
+    Handles incoming document messages.
 
-    downloaded_file = await bot.download_file(file_path)
-    save_path = os.path.join(DOWNLOADS_FOLDER, file_name)
+    Args:
+        message (types.Message): The message containing the document.
 
-    with open(save_path, "wb") as file:
-        file.write(downloaded_file.read())
+    """
+    if await get_authorize(message):
+        file_info = await bot.get_file(message.document.file_id)
+        file_path = file_info.file_path
+        file_name = message.document.file_name
 
-    await message.reply(f"Файл збережено як '{file_name}' в папці '{DOWNLOADS_FOLDER}'.")
+        downloaded_file = await bot.download_file(file_path)
+        save_path = os.path.join(DOWNLOADS_FOLDER, file_name)
+
+        with open(save_path, "wb") as file:
+            file.write(downloaded_file.read())
+
+        await message.reply(f"Файл збережено як '{file_name}' в папці '{DOWNLOADS_FOLDER}'.")
+
+
+async def get_authorize(message: types.Message) -> bool:
+    """
+    Verifies if the user's ID or username is in the list of allowed users.
+    If the user is not authorized, sends a message denying access.
+
+    Args:
+        message (types.Message): The message from the user.
+
+    Returns:
+        bool: True if the user is authorized, otherwise False.
+
+    """
+    user_id = message.from_user.id
+    username = f"@{message.from_user.username}"
+
+    if user_id in ALLOWED_USERS_IDS or username in ALLOWED_USERS_USERNAMES:
+        return True
+
+    await message.reply("Ви не маєте дозволу надсилати файли.")
+    return False
 
 
 async def main():
